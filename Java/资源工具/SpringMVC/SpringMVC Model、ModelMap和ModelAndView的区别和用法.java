7b6b5b8479e0c1873db752f58fd69523fd7665d6
JackKuang <1092465834@qq.com>
2018-02-02 00:33:04 +0800

    Spring MVC

---
+++ Java/资源工具/SpringMVC/SpringMVC Model、ModelMap和ModelAndView的区别和用法.java
@@ -0,0 +1,67 @@
+package com.learndemo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/return")
+public class LearnReturnType {
+
+    /**
+     * Model本身不能设置页面跳转的url地址别名或者物理跳转地址，那么我们可以通过控制器方法的返回值来设置跳转url
+     * 地址别名或者物理跳转地址
+     * 
+     * @param model
+     *            一个接口， 其实现类为ExtendedModelMap，继承了ModelMap类
+     * @return 跳转url地址别名或者物理跳转地址
+     */
+    @RequestMapping(value = "/index1")
+    public String index1(Model model) {
+        model.addAttribute("result", "后台返回index1");
+        return "result";
+    }
+
+    /**
+     * ModelMap对象主要用于传递控制方法处理数据到结果页面,类似于request对象的setAttribute方法的作用。 用法等同于Model
+     * 
+     * @param model
+     * @return 跳转url地址别名或者物理跳转地址
+     */
+
+    @RequestMapping(value = "/index2")
+    public String index2(ModelMap model) {
+        model.addAttribute("result", "后台返回index2");
+        return "result";
+    }
+
+    /**
+     * ModelAndView主要有两个作用 设置转向地址和传递控制方法处理结果数据到结果页面
+     * @return 返回一个模板视图对象
+     */
+    @RequestMapping(value = "/index3")
+    public ModelAndView index3() {
+        ModelAndView mv = new ModelAndView("result");
+        // ModelAndView mv = new ModelAndView();
+        // mv.setViewName("result");
+        mv.addObject("result", "后台返回index3");
+        return mv;
+    }
+
+    /**
+     * map用来存储递控制方法处理结果数据，通过ModelAndView方法返回。
+     * 当然Model,ModelMap也可以通过这种方法返回
+     * @return 返回一个模板视图对象
+     */
+    @RequestMapping(value = "/index4")
+    public ModelAndView index4() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("result", "后台返回index4");
+        return new ModelAndView("result", map);
+    }
+}
